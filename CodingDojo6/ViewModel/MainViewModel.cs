3ba98044d036e4c7dbb8322865bc2a46ed3f815b
Filip Stanojevic <filip_-96@hotmail.com>
2018-01-12 23:28:05 +0100

    Done

--- CodingDojo6/ViewModel/MainViewModel.cs
+++ CodingDojo6/ViewModel/MainViewModel.cs
@@ -4,13 +4,36 @@ using GalaSoft.MvvmLight.Ioc;
 using GalaSoft.MvvmLight.Messaging;
 using System.Collections.ObjectModel;
 using System;
+using System.Timers;
+using System.Threading;
+using System.Windows.Media.Imaging;
 
 namespace CodingDojo6.ViewModel
 {
-    
+
     public class MainViewModel : ViewModelBase
     {
         private Messenger messenger = SimpleIoc.Default.GetInstance<Messenger>();
+
+        Thread newThread;
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; RaisePropertyChanged(); }
+        }
+
+        private BitmapImage messageImage;
+
+        public BitmapImage MessageImage
+        {
+            get { return messageImage; }
+            set { messageImage = value; RaisePropertyChanged(); }
+        }
+
+
 
         public RelayCommand SwitchToOverviewBtnClicked { get; set; }
         public RelayCommand SwitchToMyToysBtnClicked { get; set; }
@@ -28,8 +51,9 @@ namespace CodingDojo6.ViewModel
 
         public MainViewModel()
         {
-            
-            SwitchToMyToysBtnClicked = new RelayCommand(()=>
+            Message = "";
+
+            SwitchToMyToysBtnClicked = new RelayCommand(() =>
             {
                 CurrentViewModel = SimpleIoc.Default.GetInstance<MyToysVm>();
             });
@@ -41,12 +65,37 @@ namespace CodingDojo6.ViewModel
 
             CurrentViewModel = SimpleIoc.Default.GetInstance<OverviewVm>();
 
-            messenger.Register<PropertyChangedMessage<ItemVm>>(this, "Write", DisplayMessage);
+            messenger.Register<PropertyChangedMessage<ItemVm>>(this, "Write", DisplayMessageThread);
         }
 
-        private void DisplayMessage(PropertyChangedMessage<ItemVm> obj)
+        private void DisplayMessageThread(PropertyChangedMessage<ItemVm> obj)
         {
-            
+            newThread = new Thread(DisplayMessage);
+            newThread.Start();
+        }
+    
+
+        private void DisplayMessage()
+        {
+            Message = "New Toy added!";
+            RaisePropertyChanged("Message");
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                MessageImage = new BitmapImage(new Uri("Images/State_Info.png", UriKind.Relative));
+                RaisePropertyChanged("MessageImage");
+            });
+
+            Thread.Sleep(3000);
+
+            Message = "";
+            RaisePropertyChanged("Message");
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                MessageImage = null;
+                RaisePropertyChanged("MessageImage");
+            });
         }
     }
 }
